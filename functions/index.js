67c03bd2cbba3a53f64b96bb489c595158efc137
Th0t-Nz1g <kevin.nzeng@gmail.com>
2019-02-18 18:58:40 +0100

    Implementation of Mail Notifier for contracts

--- functions/index.js
+++ functions/index.js
@@ -4,15 +4,57 @@ const nodemailer = require('nodemailer');
 
 admin.initializeApp(functions.config().firebase);
 
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    port: 25,
+    secure: false,
+    auth : {
+        user: 'SENDER_EMAIL',
+        pass: 'SENDER_PASSWORD'
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+});
+
+const mailOptions = {
+    from : '"Coucouya via Sage_Papel" <thot.nzeng@gmail.com>',
+    to: 'kevin.nzeng@gmail.com',
+    subject : '',
+    text: '',
+    html: ''
+
+};
+
+const end_of_contract = (mailOptions, contract) => {
+    mailOptions.subject = "Expiration du contrat \"" + contract.title + "\"";
+    mailOptions.text = "Mbolo Papel!\nLe contract \"" + contract.title + "\" expire aujourd'hui.\n\n Ngwalio ! <3";
+    transporter.sendMail(mailOptions).then(() => {
+        console.log('Mail END_OF_CONTRACT envoyé à ', mailOptions.to);
+    }).catch(err => console.log("Erreur envoi mail: ", err));
+}
+
+const six_months_notifier = (mailOptions, contract) => {
+    mailOptions.subject = "6 mois avant expiration du contrat \"" + contract.title + "\"";
+    mailOptions.text = "Mbolo Papel!\nLe contract \"" + contract.title + "\" expire dans 6 mois, le " + contract.validity + ".\n U ya Libn !\n\nNgwalio ! <3";
+    transporter.sendMail(mailOptions).then(() => {
+        console.log('SIX_MONTHS_NOTIFIER envoyé à ', mailOptions.to);
+    }).catch(err => console.log("Erreur envoi mail: ", err));
+}
+
+const three_months_notifier = (mailOptions, contract) => {
+    mailOptions.subject = "3 mois avant expiration du contrat \"" + contract.title + "\"";
+    mailOptions.text = "Mbolo Papel!\nLe contract \"" + contract.title + "\" expire dans 3 mois, le " + contract.validity + ".\n U ya Libn !\n\nNgwalio ! <3";
+    transporter.sendMail(mailOptions).then(() => {
+        console.log('THREE_MONTHS_NOTIFIER envoyé à ', mailOptions.to);
+    }).catch(err => console.log("Erreur envoi mail: ", err));
+}
+
+
 const checkValidity = (contract) => {
     const date = contract.data().validity;
     const firstRemind = contract.data().firstNotif;
     const secondRemind = contract.data().secondNotif;
-
-    /*const currentTime = (new Date()).toLocaleDateString().split('/');
-    const endDate = (new Date(date.seconds * 1000)).toLocaleDateString().split('/');
-    const remind_once = (new Date(firstRemind.seconds * 1000)).toLocaleDateString().split('/');
-    const remind_twice = (new Date(secondRemind.seconds * 1000)).toLocaleDateString().split('/');*/
 
     const currentTime = (new Date()).toLocaleDateString().split('/')
     const endDate = date.split('/');
@@ -27,30 +69,32 @@ const checkValidity = (contract) => {
     
     if (parseInt(currentTime[0]) === parseInt(endDate[1])){
         if (parseInt(currentTime[1]) === parseInt(endDate[0])){
-            if (currentTime[2] === endDate[2])
+            if (currentTime[2] === endDate[2]) {
                  console.log('Le contrat ', contract.data().title, 'arrive a expiration today')
+                 //Envoi du mail end_of_contract
+                 end_of_contract(mailOptions, contract.data());
+            }
         }
     } else if (parseInt(currentTime[0]) === parseInt(remind_once[1])){
             if (parseInt(currentTime[1]) === parseInt(remind_once[0])){
-                if (currentTime[2] === remind_once[2])
+                if (currentTime[2] === remind_once[2]){
                      console.log('Le contrat ', contract.data().title, 'arrive a expiration dans 6 mois');
+                     //Envoi du mail six_months_notifier
+                     six_months_notifier(mailOptions, contract.data());
+                }
             }
         }
         else {
             if (parseInt(currentTime[0]) === parseInt(remind_twice[1])){
                 if (parseInt(currentTime[1]) === parseInt(remind_twice[0])){
-                    if (currentTime[2] === remind_twice[2])
+                    if (currentTime[2] === remind_twice[2]) {
                          console.log('Le contrat ', contract.data().title, 'arrive a expiration dans 3 mois')
+                         //Envoi du mail three_months_reminder
+                         three_months_notifier(mailOptions, contract.data());
+                    }
                 }
             }
         }
-    
-    //console.log('current date', currentTime);
-    //console.log('validity', newDate);
-
-    //console.log('Contract ends on: ', date);
-    //console.log('First remind at : ', firstRemind);
-    //console.log('Second remind at : ', secondRemind);
 }
 
 exports.sendEmailWatcher = functions.https.onRequest((request, response) => {
